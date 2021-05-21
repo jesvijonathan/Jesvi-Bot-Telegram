757d65f87db2075161a7ad2ff1256ac8b698cd5f
Jesvi-linux <jesvi22j@gmail.com>
2021-05-21 19:43:47 +0000

    lots of changes in linux, ported lot af bat file and added swl func... ,added cit,help func start
    Signed-off-by: Jesvi-linux <jesvi22j@gmail.com>

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ path = path = str(os.path.dirname(sys.argv[0]))
 stdoutOrigin = sys.stdout
 # sys.stdout = open(path+"\\textfile.txt", "w")
 
-
+# sys.stderr = open(path+"\\logs\\log_bot_runtime.log", 'w')
 sys.stderr = open(path+"/logs/log_bot_runtime.log", 'w')
 
 
@@ -92,17 +92,17 @@ def main():
     sys.stderr = logger
 
     qinfo_cmd = ("info", "ginfo", "group", "groupinfo", "aboutgroup",
-    q            "chatinfo", "infogroup", "infochat", "user", "userinfo")
+                 q            "chatinfo", "infogroup", "infochat", "user", "userinfo")
     qadmin_cmd = ("ainfo", "adminlist", "admin", "listadmin",
-    q             "administrators", "members", "memb")
+                  q             "administrators", "members", "memb")
     qmessage_id = ("id", "minfo", "msgid", "msg",
-    q              "messageid", "msginfo", "message")
+                   q              "messageid", "msginfo", "message")
     qmute_cmd = ("mute", "shutup", "restrict")
     qspam_cmd = ("spam", "filler", "echo", "annoy")
     qpromote_cmd = ("promote", "upgrade", "prom")
     qdemote_cmd = ("demote", "depromote", "depromo", "degrade")
     qfilter_cmd = ("filter", "filt", "word", "fil",
-    q              "resetfilter", "filterreset")
+                   q              "resetfilter", "filterreset")
     qpin_cmd = ("pin", "notify", "notice", "noti")
     qset_cmd = ("set", "change", "setbio", "bio")
     qclean_cmd = ("clean", "purge", "tdel")
@@ -118,66 +118,68 @@ def main():
     qleave_cmd = ("leave", "scoot")
     qnote_cmd = ("note", "notes")
     qwarn_cmd = {"warnset", "warnclear", "warnreset", "forgive",
-    q            "withdraw", "warninfo", "warnlist", "allwarns", "warnremove"}
+                 q            "withdraw", "warninfo", "warnlist", "allwarns", "warnremove"}
     qrules_cmd = {"rules", "setrules", "setrule", "rule"}
     qsearch_cmd = {"search", "google", "usearch"}
     qtranslate_cmd = {"trans", "translate"}
     qcreate_base = {"createbase"}
     qsql_cmd = {"sqlon", "sqloff"}
+
+
 q
-    qdp.add_handler(CommandHandler(create_base, database.create_base))
-    qdp.add_handler(CommandHandler(sql_cmd, sqlof))
-    qdp.add_handler(CommandHandler("ipltoday", fun.cricket))
-    qdp.add_handler(CommandHandler("iplupdate", fun.cricket))
-    qdp.add_handler(CommandHandler(search_cmd, ai.search))
-    qdp.add_handler(CommandHandler(translate_cmd, ai.translate))
-    qdp.add_handler(CommandHandler("warn", filter.warn_strike))
-    qdp.add_handler(CommandHandler(warn_cmd, filter.warn_set))
-    qdp.add_handler(CommandHandler(rules_cmd, edit.rules))
-    qdp.add_handler(CommandHandler("fbi", fun.fbi_joke))
-    qdp.add_handler(CommandHandler("start", welcome.start_func))
-    qdp.add_handler(CommandHandler("cdsync", welcome.dat))
-    qdp.add_handler(CommandHandler("net", database.net))
-    qdp.add_handler(CommandHandler(note_cmd, notes.notes))
-    qdp.add_handler(CommandHandler(boom_cmd, spams.boom))
-    qdp.add_handler(CommandHandler("owner", welcome.owner))
-    qdp.add_handler(CommandHandler(about_cmd, welcome.about))
-    qdp.add_handler(CommandHandler(rip_cmd, ban.rip))
-    qdp.add_handler(CommandHandler(kick_cmd, ban.kick))
-    qdp.add_handler(CommandHandler(ban_cmd, ban.ban))
-    qdp.add_handler(CommandHandler(unban_cmd, ban.unban))
-    qdp.add_handler(CommandHandler(leave_cmd, ban.leave))
-    qdp.add_handler(CommandHandler(pin_cmd, edit.pin))
-    qdp.add_handler(CommandHandler("unpin", edit.unpin))
-    qdp.add_handler(CommandHandler(set_cmd, edit.set_))
-    qdp.add_handler(CommandHandler(promote_cmd, promote.promote))
-    qdp.add_handler(CommandHandler(demote_cmd, promote.depromote))
-    qdp.add_handler(CommandHandler(spam_cmd, spams.spam_check))
-    qdp.add_handler(CommandHandler(mute_cmd, mute.mute))
-    qdp.add_handler(CommandHandler("unmute", mute.unmute))
-    qdp.add_handler(CommandHandler((info_cmd), info.info))
-    qdp.add_handler(CommandHandler(admin_cmd, info.admin_list))
-    qdp.add_handler(CommandHandler(message_id, info.msg_id))
-    qdp.add_handler(CommandHandler(filter_cmd, filter.message_filter))
-    qdp.add_handler(CommandHandler(clean_cmd, delete.clean))
-    qdp.add_handler(CommandHandler(delete_cmd, delete.delete))
-    qdp.add_handler(CommandHandler(silent_delete_cmd, delete.silent_delete))
-    qdp.add_handler(CommandHandler("lock", delete.lock))
-    qdp.add_handler(CommandHandler("unlock", delete.unlock))
+qdp.add_handler(CommandHandler(create_base, database.create_base))
+qdp.add_handler(CommandHandler(sql_cmd, sqlof))
+qdp.add_handler(CommandHandler("ipltoday", fun.cricket))
+qdp.add_handler(CommandHandler("iplupdate", fun.cricket))
+qdp.add_handler(CommandHandler(search_cmd, ai.search))
+qdp.add_handler(CommandHandler(translate_cmd, ai.translate))
+qdp.add_handler(CommandHandler("warn", filter.warn_strike))
+qdp.add_handler(CommandHandler(warn_cmd, filter.warn_set))
+qdp.add_handler(CommandHandler(rules_cmd, edit.rules))
+qdp.add_handler(CommandHandler("fbi", fun.fbi_joke))
+qdp.add_handler(CommandHandler("start", welcome.start_func))
+qdp.add_handler(CommandHandler("cdsync", welcome.dat))
+qdp.add_handler(CommandHandler("net", database.net))
+qdp.add_handler(CommandHandler(note_cmd, notes.notes))
+qdp.add_handler(CommandHandler(boom_cmd, spams.boom))
+qdp.add_handler(CommandHandler("owner", welcome.owner))
+qdp.add_handler(CommandHandler(about_cmd, welcome.about))
+qdp.add_handler(CommandHandler(rip_cmd, ban.rip))
+qdp.add_handler(CommandHandler(kick_cmd, ban.kick))
+qdp.add_handler(CommandHandler(ban_cmd, ban.ban))
+qdp.add_handler(CommandHandler(unban_cmd, ban.unban))
+qdp.add_handler(CommandHandler(leave_cmd, ban.leave))
+qdp.add_handler(CommandHandler(pin_cmd, edit.pin))
+qdp.add_handler(CommandHandler("unpin", edit.unpin))
+qdp.add_handler(CommandHandler(set_cmd, edit.set_))
+qdp.add_handler(CommandHandler(promote_cmd, promote.promote))
+qdp.add_handler(CommandHandler(demote_cmd, promote.depromote))
+qdp.add_handler(CommandHandler(spam_cmd, spams.spam_check))
+qdp.add_handler(CommandHandler(mute_cmd, mute.mute))
+qdp.add_handler(CommandHandler("unmute", mute.unmute))
+qdp.add_handler(CommandHandler((info_cmd), info.info))
+qdp.add_handler(CommandHandler(admin_cmd, info.admin_list))
+qdp.add_handler(CommandHandler(message_id, info.msg_id))
+qdp.add_handler(CommandHandler(filter_cmd, filter.message_filter))
+qdp.add_handler(CommandHandler(clean_cmd, delete.clean))
+qdp.add_handler(CommandHandler(delete_cmd, delete.delete))
+qdp.add_handler(CommandHandler(silent_delete_cmd, delete.silent_delete))
+qdp.add_handler(CommandHandler("lock", delete.lock))
+qdp.add_handler(CommandHandler("unlock", delete.unlock))
 q
-    qdp.add_handler(CommandHandler("help", welcome._help))
+qdp.add_handler(CommandHandler("help", welcome._help))
 q
-    qdp.add_handler(CommandHandler("cit", cit.cit))
+qdp.add_handler(CommandHandler("cit", cit.cit))
 q
-    qdp.add_handler(CommandHandler("oof", fun.oof))
+qdp.add_handler(CommandHandler("oof", fun.oof))
 q
-    qdp.add_handler(MessageHandler(
+qdp.add_handler(MessageHandler(
     q    Filters.status_update.new_chat_members, welcome.greet))
-    qdp.add_handler(MessageHandler(
+qdp.add_handler(MessageHandler(
     q    Filters.status_update.left_chat_member, welcome.farewell))
-    qdp.add_handler(MessageHandler(Filters.all, send))
-    qupdater.start_polling()
-    qupdater.idle()
+qdp.add_handler(MessageHandler(Filters.all, send))
+qupdater.start_polling()
+qupdater.idle()
 
 
 if __name__ == '__main__':
